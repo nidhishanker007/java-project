4456e987084c766ad944c973977d20d1737bba9a
Root User <shankernidhi@gmail.com>
2018-03-06 18:43:01 +0000

    initial java commit

---
+++ src/Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+public int length;
+public int width;
+public Rectangle(int length, int width){
+this.length-length;
+this.width=width;
+}
+public int getArea() {
+return length * width;
+}
+}
+
